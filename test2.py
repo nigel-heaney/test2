677092c378d4ca513cd3cd0a6b88bd9cfb3638ad
Nigel Heaney <nigelheaney@rentalcars.com>
2018-04-05 13:55:41 +0100

    update

--- test2.py
+++ test2.py
@@ -4,38 +4,58 @@ import logging
 
 
 class logit():
+    """ this
+    """
     def __init__(self):
+        """
+        This
+        """
         self.logfilename = "/var/log/output.log"
         self.dateformat = "%Y-%m-%d_%H-%M-%S"
         self.default_log_level = logging.DEBUG
         self.filemode = 'w'  # a,w
 
     def startlog(self):
+        """This
+        """
         logging.basicConfig(filename=self.logfilename, level=self.default_log_level, datefmt=self.dateformat,
                             format='%(asctime)s:%(levelname)8s: %(message)s', filemode=self.filemode)
         logging.info("Logging started")
 
     def loginfo(self, output):
+        """This
+        """
         logging.info(output)
 
     def logwarn(self, output):
+        """This
+        """
         logging.warning(output)
 
     def logcritical(self, output):
+        """This"""
         logging.critical(output)
 
     def logerror(self, output):
+        """This
+        """
         logging.error(output)
 
     def logdebug(self, output):
+        """This
+        """
         logging.debug(output)
 
 
-if __name__ == '__main__':
+def main():
     x = logit()
     x.logfilename = "./test.log"
     x.startlog()
     x.logwarn("test")
     x.logcritical("test")
     x.loginfo("test")
-x.logdebug("test")
+    x.logdebug("test")
+
+
+if __name__ == '__main__':
+    main()
